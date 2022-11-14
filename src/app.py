00bb35a33028989f241ecf14ece0411f91ad79a5
iperez <iperez@lynxview.es>
2022-11-14 23:03:43 +0100

    Scripts reconocimiento de voz

--- src/app.py
+++ src/app.py
@@ -9,12 +9,13 @@ app = Flask("servicios")
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'wav'}
 
-@app.route('/', methods=(['POST']))
+@app.route('/')
 def main():
-    return render_template('templates/index.html')
+    return render_template('index.html')
     
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(
         host="0.0.0.0",
         port=5000
