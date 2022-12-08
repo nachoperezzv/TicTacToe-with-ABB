83148c63d269733e5737d2e6fb8645ec27c35460
iperez <iperez@lynxview.es>
2022-12-08 17:03:01 +0100

    Añadido custom exception y logging

---
+++ logger.py
@@ -0,0 +1,8 @@
+import os.path
+
+def getFullPatch():
+    try:
+        dir = os.path.dirname(os.path.realpath(__file__))
+    except Exception as e:
+        print("fallo en el path")
+    return dir
