7ce62b1af65bbc74a70e1150d613d4e09a0d5141
iperez <iperez@lynxview.es>
2022-12-13 12:22:36 +0100

    paths de los utils corregido

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,11 @@
+from utils.tcp import TCP
 from utils.player import Player
 from utils.tablero import Tablero
+from utils.threeinRow import ThreeInRow
 
 from utils.utilities import (
     get_from_request,
     set_response
 )
 
-from utils.threeinRow import ThreeInRow
+
